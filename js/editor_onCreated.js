1f8495153cd65851e11b2494c94bf82d338da695
lincaiyong <lincaiyong@bytedance.com>
2025-11-06 14:27:59 +0800

    page

--- js/editor_onCreated.js
+++ js/editor_onCreated.js
@@ -1,12 +1,10 @@
 function onCreated() {
-    const queryString = window.location.search;
+    const queryString = window.location.search; // ?language=javascript&...
     const urlParams = new URLSearchParams(queryString);
-    if (urlParams.has('code')) {
-        const codeValue = urlParams.get('code');
-        const decodedValue = decodeURIComponent(codeValue);
+    page.util.fetch('<base_url>/code/get' + queryString).then(v => {
         const options = {
-            value: decodedValue,
-            language: 'go',
+            value: v,
+            language: urlParams.get('language') || 'go',
             theme: 'vs',
             automaticLayout: true,
             lineNumbers: 'on', // 'off'
@@ -19,23 +17,5 @@ function onCreated() {
             // suggestOnTriggerCharacters: false,
         };
         this._editor = monaco.editor.create(this.ref, options);
-    } else {
-        page.util.fetch('<base_url>/code/get' + queryString).then(v => {
-            const options = {
-                value: v,
-                language: 'go',
-                theme: 'vs',
-                automaticLayout: true,
-                lineNumbers: 'on', // 'off'
-                minimap: {
-                    enabled: false,
-                },
-                readOnly: true,
-                // fontFamily: '',
-                // glyphMargin: false,
-                // suggestOnTriggerCharacters: false,
-            };
-            this._editor = monaco.editor.create(this.ref, options);
-        }).catch(err => page.log.error(err));
-    }
+    }).catch(err => page.log.error(err));
 }
