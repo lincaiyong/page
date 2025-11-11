f676acc09d43225c589143c7080704c1fb5ffa3d
lincaiyong <lincaiyong@bytedance.com>
2025-11-11 13:20:09 +0800

    page

---
+++ com/color.go
@@ -0,0 +1,26 @@
+package com
+
+import "fmt"
+
+func Color(r, g, b int) string {
+	return fmt.Sprintf("'#%02x%02x%02x'", r, g, b)
+}
+
+const (
+	ColorBlack   = "'black'"   // 黑色
+	ColorSilver  = "'silver'"  // 银色
+	ColorGray    = "'gray'"    // 灰色
+	ColorWhite   = "'white'"   // 白色
+	ColorMaroon  = "'maroon'"  // 栗色
+	ColorRed     = "'red'"     // 红色
+	ColorPurple  = "'purple'"  // 紫色
+	ColorMagenta = "'magenta'" // 洋红色
+	ColorGreen   = "'green'"   // 绿色
+	ColorLime    = "'lime'"    // 酸橙色
+	ColorOlive   = "'olive'"   // 橄榄色
+	ColorYellow  = "'yellow'"  // 黄色
+	ColorNavy    = "'navy'"    // 海军蓝
+	ColorBlue    = "'blue'"    // 蓝色
+	ColorTeal    = "'teal'"    // 蓝绿色
+	ColorCyan    = "'cyan'"    // 水绿色/青色
+)
