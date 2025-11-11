83e686da4ab3cdc34c764cf7975023bf0993ce1f
lincaiyong <lincaiyong@bytedance.com>
2025-11-11 13:22:21 +0800

    page

--- example/bar_test.go
+++ example/bar_test.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/lincaiyong/daemon/common"
 	"github.com/lincaiyong/page"
+	"github.com/lincaiyong/page/com"
 	"github.com/lincaiyong/page/com/bar"
 	"github.com/lincaiyong/page/com/button"
 	"github.com/lincaiyong/page/com/compare"
@@ -22,18 +23,18 @@ func TestBar(t *testing.T) {
 			r.GET("/", func(c *gin.Context) {
 				comp := root.Root("", div.Div().Contains(
 					div.Div().W("next.x").Contains(
-						editor.Editor().X("20").Y("0").W("800").H("next.y - .y").BackgroundColor("'blue'"),
-						bar.HBar().BackgroundColor("'blue'").Opacity("0.1").Y("parent.h/2").W("parent.w"),
-						div.Div().X("20").Y("prev.y2").W("800").H("parent.h-prev.y2").BackgroundColor("'yellow'").Contains(
+						editor.Editor().X("20").Y("0").W("800").H("next.y - .y").BackgroundColor(com.ColorBlue),
+						bar.HBar().BackgroundColor(com.ColorBlue).Opacity("0.1").Y("parent.h/2").W("parent.w"),
+						div.Div().X("20").Y("prev.y2").W("800").H("parent.h-prev.y2").BackgroundColor(com.ColorYellow).Contains(
 							text.Text("'hello world!'").H("200"),
 						),
 					),
-					bar.VBar().X("parent.w/2").BackgroundColor("'blue'").Opacity("0.1"),
+					bar.VBar().X("parent.w/2").BackgroundColor(com.ColorBlue).Opacity("0.1"),
 					div.Div().X("prev.x2").W("parent.w-prev.x2").Contains(
-						compare.Compare().Y("0").H("next.y").BackgroundColor("'red'"),
-						bar.HBar().BackgroundColor("'blue'").Opacity("0.1").Y("parent.h/2").W("parent.w"),
-						div.Div().Y("prev.y2").W("40").H("40").BackgroundColor("'green'"),
-						button.Button().Icon("'svg/el/folder.svg'").X("prev.x2").Y("prev.y2 + 100").W("40").H("40"),
+						compare.Compare().Y("0").H("next.y").BackgroundColor(com.ColorRed),
+						bar.HBar().BackgroundColor(com.ColorBlue).Opacity("0.1").Y("parent.h/2").W("parent.w"),
+						div.Div().Y("prev.y2").W("40").H("40").BackgroundColor(com.ColorGreen),
+						button.Button().Icon(com.SvgElAddLocation).X("prev.x2").Y("prev.y2 + 100").W("40").H("40"),
 					),
 				))
 				page.MakePage(c, "debug", comp, baseUrl)
